5eb04b4b7be26da8ca45366ac5de8dc904054d85
anupaprabhasara <info@anupa.lk>
2025-05-09 15:17:30 +0530

    Done

---
+++ src/main/java/com/caredesk/servlet/UserLogoutServlet.java
@@ -0,0 +1,20 @@
+package com.caredesk.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class UserLogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        response.sendRedirect(request.getContextPath() + "/");
+    }
+}
